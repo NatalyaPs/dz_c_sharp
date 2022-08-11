8e17ed7aa13a2b3ac2b4f990d3871711927f9d77
NatalyaPs <n09051@yandex.ru>
2022-08-11 13:06:07 +0300

    улучшила алгоритмы в 4 и 8 задаче по рекомендации преподавателя

--- task4/Program.cs
+++ task4/Program.cs
@@ -3,7 +3,7 @@
 //44 5 78 -> 78
 //22 3 9 -> 22
 
-Console.WriteLine("введите первое число");
+/*Console.WriteLine("введите первое число");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
@@ -13,6 +13,25 @@ int c = Convert.ToInt32(Console.ReadLine());
 int max = 0;
 
 if (max<a) max=a;
+if (max<b) max=b;
+if (max<c) max=c;
+
+Console.WriteLine(max);*/
+
+//в впервом решении не получается с отрицательными числами. Ответ выдает 0.
+// преподаватель Алексей Свищев посоветовал поменять max. Задать это значение 1му числу.
+//это позволит избавиться от одной конструкции if и обрабатывать отрицательные числа.
+// Пробую:
+
+Console.WriteLine("введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
 if (max<b) max=b;
 if (max<c) max=c;
 
