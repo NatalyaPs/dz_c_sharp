78eac77ea475ad3db49117ababa4b5da5083c93b
NatalyaPs <n09051@yandex.ru>
2022-09-06 17:46:31 +0300

    add tasks 64, 66, 68

---
+++ task64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите значение числа N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbers (int number)
+{
+    if (number==0)
+    {
+        return;
+    }
+    Console.Write($"{number}");   //если поменять местами эти 2 строки, то
+    PrintNumbers(number-1);       //изменится порядок вывода чисел
+}
+PrintNumbers(n);
---
+++ task68/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 29
+
+Console.WriteLine("Введите число m: ");
+uint m = Convert.ToUInt32(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+uint n = Convert.ToUInt32(Console.ReadLine());
+
+uint Akkerman (uint m, uint n)
+{
+    if (m == 0)
+    {
+        return n+1; 
+    } 
+    else if (m > 0 && n == 0)
+    {
+        return Akkerman(m- 1, 1);
+    }
+    else
+    //else if (m > 0 && n > 0)
+    {
+        return Akkerman(m-1, Akkerman(m, n-1));
+    }
+}
+//Akkerman(m,n);
+Console.WriteLine(Akkerman(m, n));
