84665fd7a3614f23cfbb1a5f72b5bc5899769f68
NatalyaPs <n09051@yandex.ru>
2022-09-08 19:48:43 +0300

    add new task 62

--- task62/Program.cs
+++ task62/Program.cs
@@ -5,67 +5,52 @@
 // 11 16 15 06
 // 10 09 08 07
 
+
 int n = 4;
 int[,] matrix = new int[n, n];
 int num = 1;
-//int shift = 0;
 
 for (int shift = 0; shift < n - 2; shift++)
 {
-    Console.WriteLine();
-    for (int i = 0 + shift; i < n - shift; i++)  // верхняя строка. слева-направо  
+    for (int j = 0 + shift; j < n - shift; j++)  //верхняя строка слева-направо
     {
-        for (int j = 0 + shift; j < n - shift; j++)
-        {
-            matrix[0 + shift, j] = num;
-            num++;
-            //shift++;
-            Console.Write(matrix[i, j] + "\t");
-        }
-        Console.WriteLine();
+        matrix[0 + shift, j] = num;
+        num++;
     }
     num--;
 
-    Console.WriteLine();
-    for (int j = 0 + shift; j < n - shift; j++)  //правый столбец. сверху-вниз
+    for (int i = 0 + shift; i < n - shift; i++)  //правый столбец сверху-вниз
     {
-        for (int i = 0 + shift; i < n - shift; i++)
-        {
-            matrix[j, n-1 - shift] = num;
-            num++;
-            //shift++;
-            Console.Write(matrix[j, i] + "\t");
-        }
-        Console.WriteLine();
+        matrix[i, n - 1 - shift] = num;
+        num++;
     }
     num--;
 
-    Console.WriteLine();
-    for (int i = n - 1 - shift; i >= 0 + shift; i--)  //нижняя строка. справа-налево
+    for (int j = n - 1 - shift; j >= 0 + shift; j--)  //нижняя строка справа-налево
     {
-        for (int j = n - 1 - shift; j >= 0 + shift; j--)
-        {
-            matrix[n-1 - shift, j] = num;
-            num++;
-            //shift++;
-            Console.Write(matrix[i, j] + "\t");
-        }
-        Console.WriteLine();
+        matrix[n - 1 - shift, j] = num;
+        num++;
     }
     num--;
 
-    Console.WriteLine();
-    for (int j = n - 1 - shift; j >= 1 + shift; j--)  //левый столбец. снизу вверх
+    for (int i = n - 1 - shift; i >= 1 + shift; i--)  //левый столбец снизу вверх
     {
-        for (int i = n - 1 - shift; i >= 0 + shift; i--)
-        {
-            matrix[j, 0 + shift] = num;
-            num++;
-            //shift++;
-            Console.Write(matrix[j, i] + "\t");
-        }
-        Console.WriteLine();
+        matrix[i, 0 + shift] = num;
+        num++;
     }
-    num--;
 }
 
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintMatrix(matrix);
